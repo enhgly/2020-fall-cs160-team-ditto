b5b2d71e2ce088c40f431d6148b0bfa61ee4da38
Arlan Prado <arlanjprado2016@gmail.com>
2020-09-25 21:16:36 -0700

    Springboot demo added

---
+++ demo/demo/src/test/java/com/Grouped/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grouped.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
