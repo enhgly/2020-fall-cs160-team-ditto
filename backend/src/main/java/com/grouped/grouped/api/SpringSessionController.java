27ebb10aa54c20144cbaaa3da56def2d74583674
Arlan Prado <arlanjprado2016@gmail.com>
2020-11-13 17:23:53 -0800

    httpsessions attempt
    need to implement ajax, for login and then into the home page. start session at login, end when a "logout" button is clicked or with a timeout

---
+++ backend/src/main/java/com/grouped/grouped/api/SpringSessionController.java
@@ -0,0 +1,56 @@
+package com.grouped.grouped.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("api/session")
+@Controller
+public class SpringSessionController {
+
+    @GetMapping("/")
+    public String process(Model model, HttpSession session) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+        model.addAttribute("sessionMessages", messages);
+
+        return "index";
+    }
+
+    @PostMapping("/start")
+    public String startSession(@RequestParam("user_email") String user_email, HttpServletRequest request){
+        request.getSession().setAttribute("user_email", user_email);
+        return "redirect:/";
+    }
+    @PostMapping("/persistMessage")
+    public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        }
+        messages.add(msg);
+        request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+        return "redirect:/";
+    }
+
+    @PostMapping("/destroy")
+    public String destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/";
+    }
+}
